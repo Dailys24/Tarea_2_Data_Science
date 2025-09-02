9870d8130c1b727d64e580831b6ba25851ef4421
NicolasRosalesGrandon <164436493+NicolasRosalesGrandon@users.noreply.github.com>
2025-09-02 18:28:09 -0400

    Update Problema5.py

--- Problema5.py
+++ Problema5.py
@@ -1,1 +1,15 @@
+#[5] Listado ordenado (de mayor a menor) de los estudiantes según su promedio.")
+
+import pandas as pd 
+
+from Datos import datos #importa lista de diccionarios 
+
+def problema5(): 
+    df_alumnos=pd.DataFrame(datos)
+    df_alumnos['promedio'] = df_alumnos['notas'].apply(lambda notas : sum(notas) / len (notas) )
+    def_ordenado = df_alumno[['nombre', 'promedio']].sort_values(by='promedio', ascending=False )
+
+    print("------------- Listado de estudiantes según promedio ----------------")
+    print(df_ordenado.round(1).to_string(index=False))
+
 
