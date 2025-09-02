5e18f4fb490dddf58988c533ca674e5272b2e6b5
Dailys24 <angelo.andresgl@gmail.com>
2025-09-02 00:35:10 -0400

    Agregar funciones

--- Main.py
+++ Main.py
@@ -7,6 +7,9 @@ import os
 from Datos import datos
 from Problema1 import problema1
 from Problema2 import problema2
+from Problema3 import problema3
+from Problema4 import problema4
+from Problema5 import problema5
 
 # unción principal que contiene el menú
 def main():
@@ -48,18 +51,18 @@ def main():
             elif op == 3:
                 print("------------------------------------------------------------------------------------")
                 print(" es:")
-                #Llamamos a la función del problema 3
+                problema3() #Llamamos a la función del problema 3
                 print("------------------------------------------------------------------------------------")
             elif op == 4:
                 print("------------------------------------------------------------------------------------")
                 print(" es:")
-                #Llamamos a la función del problema 4
+                problema4() #Llamamos a la función del problema 4
                 print("------------------------------------------------------------------------------------")
             
             elif op == 5:
                 print("------------------------------------------------------------------------------------")
                 print(" es:")
-                #Llamamos a la función del problema 5
+                problema5() #Llamamos a la función del problema 5
                 print("------------------------------------------------------------------------------------")
             
             elif op == 6:
