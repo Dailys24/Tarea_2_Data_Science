0191bab3650b4591d3d13aa4890b95e63f034983
Dailys24 <angelo.andresgl@gmail.com>
2025-09-03 23:59:47 -0400

    Update Problema1.py

--- Problema1.py
+++ Problema1.py
@@ -10,18 +10,9 @@ def problema1():
     #Calcular el promedio de cada alumno
     df_alumnos['promedio']=df_alumnos['notas'].apply(lambda notas: sum(notas) / len(notas))
 
-    #Calcular el promedio general
-    promedio_general=df_alumnos['promedio'].mean()
-
     #Promedio más alto y más bajo
     alumno_nota_max=df_alumnos.loc[df_alumnos['promedio'].idxmax()]
     alumno_nota_min=df_alumnos.loc[df_alumnos['promedio'].idxmin()]
 
-    print("------------- Resumen de Notas del Problema 1 -------------")
-    print(f"Promedio general del curso: {promedio_general:.1f}")
-    print("------------------------------------------------------------------------------------")
-    print(f"El alumno con el promedio más alto es {alumno_nota_max['nombre']} con un promedio de: {alumno_nota_max['promedio']:.1f}")
-    print("------------------------------------------------------------------------------------")
-    print(f"El alumno con el promedio más bajo es {alumno_nota_min['nombre']} con un promedio de: {alumno_nota_min['promedio']:.1f}")
     print("\n------------- Promedios -------------")
     print(df_alumnos[['nombre', 'promedio']].round(1))
